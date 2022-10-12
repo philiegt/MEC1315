8aa98f012cf86b396a176bd8e0aa679a14b806b4
thiagomgomes <115653235+thiagomgomes@users.noreply.github.com>
2022-10-12 12:27:15 -0400

    Add files via upload

---
+++ PROJET1.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 12 12:25:52 2022
+
+@author: Thiago
+"""
+
+## --------------------------- CODE POUR LE PROJET 1 DE MEC1315 -----------------------------------#
